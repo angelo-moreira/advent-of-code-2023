63e586461176ae3d882553ba9b64be8b8bb550f7
Angelo Moreira <angelo.moreira@gmail.com>
2023-12-04 23:59:04 +0000

    day 1 - solution 2 (some of the worst code I've written in my life

--- src/day1.clj
+++ src/day1.clj
@@ -46,3 +46,81 @@
     list_to_ints)))
 
 ;; (day1_1)
+
+(defn char_to_num_back [find, num_char, num]
+  (let [i (str/last-index-of find num_char)]
+    (if (nil? i)
+      nil
+      {:word find
+       :replace_word num_char
+       :replace_with num
+       :found_at i
+       :found_until (+ i (count num_char))})))
+
+(defn char_to_num_forward [find, num_char, num]
+  (let [i (str/index-of find num_char)]
+    (if (nil? i)
+      nil
+      {:word find
+       :replace_word num_char
+       :replace_with num
+       :found_at i
+       :found_until (+ i (count num_char))})))
+
+(def translations [["one"   "1"]
+                   ["two"   "2"]
+                   ["three" "3"]
+                   ["four" "4"]
+                   ["five" "5"]
+                   ["six" "6"]
+                   ["seven" "7"]
+                   ["eight" "8"]
+                   ["nine" "9"]])
+
+(defn fill_indexes [x]
+  (flatten
+   (merge
+    (remove nil? (map (fn [[k v]] (char_to_num_back x k v)) translations))
+    (remove nil? (map (fn [[k v]] (char_to_num_forward x k v)) translations)))))
+
+(fill_indexes "fourzhpnphmq52r813four")
+
+(defn find_words [word]
+  (let [filled (remove (fn [x] (nil? x)) (fill_indexes word))]
+    (if (empty? filled)
+      [{:word word
+        :replace_word ""
+        :replace_with ""
+        :found_at 0
+        :found_until 0}]
+      filled)))
+
+(defn sort_indexes [word]
+  (sort-by (fn [x] (x :found_at)) word))
+
+(defn wtf [word]
+  (str/join
+   ""
+   [(subs (word :word) 0 (word :found_at))
+    (word :replace_with)
+    (subs (word :word) (word :found_until) (count (word :word)))]))
+
+
+(defn change_word [word_list]
+  (str/join
+   ""
+   (map wtf word_list)))
+
+(def
+  words
+  (load_file "resources/day1_2_prod.txt"))
+
+(->>
+ words
+ (map find_words)
+ (map sort_indexes)
+ (map change_word)
+ (strs_to_numbers)
+ (list_to_two_digits)
+ (list_to_ints)
+ (reduce +))
